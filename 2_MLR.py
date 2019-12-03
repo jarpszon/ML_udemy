d45994fb5bad4bf06acd75cd5592b01fb4b8980d
jarpszon <39600171+jarpszon@users.noreply.github.com>
2019-12-03 16:39:08 +0100

    test

--- 2_MLR.py
+++ 2_MLR.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  1 15:30:04 2019
-
-@author: Użytkownik
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 
 import pandas as pd
 import numpy as np
 import matplotlib as plt
 
 #import dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv('2_MLR_50_Startups.csv')
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,4].values
 
@@ -50,10 +37,15 @@ regressor.fit(X_train, y_train)
 # Prediciting Test set result
 y_pred = regressor.predict(X_test)
 
+# Visualisation
+plt.pyplot.plot(y_pred) #, colour='red')
+plt.pyplot.plot(y_test)
+plt.pyplot.show()
 
-
-
-
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+# adding column with 1s to the X 
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1 )
 
 
 
